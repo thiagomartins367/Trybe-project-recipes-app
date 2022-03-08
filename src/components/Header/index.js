2c3ed18ae4d9cb6e4421ffa60279b61a1c0dbe79
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2022-03-08 18:02:13 -0300

    Requisito 11

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 
 export default function Header() {
+  const history = useHistory();
   return (
     <header style={ { height: '58px' } }>
       <button
-        type="submit"
+        type="button"
         data-testid="profile-top-btn"
+        onClick={ () => history.push('/profile') }
       >
         <img src={ profileIcon } alt="profile icon" />
       </button>
