ee75a89f5a9447c7d60522e807843c56a0be798f
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2022-03-08 17:58:01 -0300

    Requisito 10

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import profileIcon from '../../images/profileIcon.svg';
+import searchIcon from '../../images/searchIcon.svg';
 
 export default function Header() {
   return (
     <header style={ { height: '58px' } }>
-      <button type="submit" data-testid="profile-top-btn">icon</button>
+      <button
+        type="submit"
+        data-testid="profile-top-btn"
+      >
+        <img src={ profileIcon } alt="profile icon" />
+      </button>
 
       <h1 data-testid="page-title">Foods</h1>
 
-      <button type="button" data-testid="search-top-btn">search</button>
+      <button
+        type="button"
+        data-testid="search-top-btn"
+      >
+        <img src={ searchIcon } alt="search icon" />
+      </button>
     </header>
   );
 }
