54c57082bfa1ea16b6404b0a12d840c25a7d748d
Thais Luza <thaisluza21@hotmail.com>
2022-03-08 18:16:04 -0300

    requisitos 85, 86, 87

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Profile() {
+  const history = useHistory();
   const [email, setEmail] = useState('');
   useEffect(() => {
     setEmail(JSON.parse(localStorage.getItem('user')));
@@ -9,9 +11,34 @@ function Profile() {
   return (
     <div>
       <h3 data-testid="profile-email">{email.email}</h3>
-      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-      <button type="button" data-testid="profile-logout-btn">Logout</button>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => {
+          history.push('/done-recipes');
+        } }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => {
+          history.push('/favorite-recipes');
+        } }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+      >
+        Logout
+      </button>
     </div>
   );
 }
