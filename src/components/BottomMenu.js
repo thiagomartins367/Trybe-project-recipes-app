e70347b75cf76723e28275c8f16704d9664c59e1
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-03-08 14:47:15 -0300

    Requsito 20 feito.

--- src/components/BottomMenu.js
+++ src/components/BottomMenu.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 const BottomMenu = () => {
   return (
     <footer data-testid="footer">
       <Link to="">
-        <img src="" alt="Drinks" data-testid="drinks-bottom-btn" />
+        <img src={ drinkIcon } alt="Drinks" data-testid="drinks-bottom-btn" />
       </Link>
       <Link to="">
-        <img src="" alt="Explorar" data-testid="explore-bottom-btn" />
+        <img src={ exploreIcon } alt="Explorar" data-testid="explore-bottom-btn" />
       </Link>
       <Link to="">
-        <img src="" alt="Food" data-testid="food-bottom-btn" />
+        <img src={ mealIcon } alt="Food" data-testid="food-bottom-btn" />
       </Link>
     </footer>
   );
