d2dd3dc0627218d80dbba123923da74b42ea7e3b
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-03-08 15:03:49 -0300

    Alterações feitas para corrigir erros de lint.

--- src/components/BottomMenu.js
+++ src/components/BottomMenu.js
@@ -5,17 +5,17 @@ import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 const BottomMenu = () => (
-    <footer data-testid="footer">
-      <Link to="/drinks">
-        <img src={ drinkIcon } alt="Drinks" data-testid="drinks-bottom-btn" />
-      </Link>
-      <Link to="/explore">
-        <img src={ exploreIcon } alt="Explorar" data-testid="explore-bottom-btn" />
-      </Link>
-      <Link to="/foods">
-        <img src={ mealIcon } alt="Food" data-testid="food-bottom-btn" />
-      </Link>
-    </footer>
+  <footer data-testid="footer">
+    <Link to="/drinks">
+      <img src={ drinkIcon } alt="Drinks" data-testid="drinks-bottom-btn" />
+    </Link>
+    <Link to="/explore">
+      <img src={ exploreIcon } alt="Explorar" data-testid="explore-bottom-btn" />
+    </Link>
+    <Link to="/foods">
+      <img src={ mealIcon } alt="Food" data-testid="food-bottom-btn" />
+    </Link>
+  </footer>
 );
 
 export default BottomMenu;
