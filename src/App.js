5fe9eff8856eaef654febeaa905595971f52bb48
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-03-11 16:01:52 -0300

    Alterações feitas para corrigir erros de lint.

--- src/App.js
+++ src/App.js
@@ -15,22 +15,22 @@ function App() {
     <main>
       <ContextProvider>
         <Switch>
-          {/* <Route path="/foods/:id-da-receita" component={ } />
-          <Route path="/drinks/:id-da-receita" component={ } /> */}
           <Route path="/foods" component={ RecipesScreen } />
           <Route path="/drinks" component={ RecipesScreen } />
           <Route exact path="/explore" component={ Explore } />
           <Route path="/profile" component={ Profile } />
-          {/* <Route path="/foods/:id-da-receita/in-progress" component={ } />
+          <Route path="/explore/foods" component={ ExploreFoods } />
+          <Route path="/explore/drinks" component={ ExploreDrinks } />
+          <Route exact path="/" component={ Login } />
+          {/* <Route path="/foods/:id-da-receita" component={ } />
+          <Route path="/drinks/:id-da-receita" component={ } />
+          <Route path="/foods/:id-da-receita/in-progress" component={ } />
           <Route path="/drinks/:id-da-receita/in-progress" component={ } />
-          <Route path="/explore/foods" component={ } />
-          <Route path="/explore/drinks" component={ } />
           <Route path="/explore/foods/ingredients" component={ } />
           <Route path="/explore/drinks/ingredients" component={ } />
           <Route path="/explore/foods/nationalities" component={ } />
           <Route path="/done-recipes" component={ } />
           <Route path="/favorite-recipes" component={ } /> */}
-          <Route exact path="/" component={ Login } />
         </Switch>
       </ContextProvider>
     </main>
