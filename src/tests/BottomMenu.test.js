5c7455b6ac4acef64d71f5cf7189c567cc0e642d
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-03-14 18:41:27 -0300

    Alterações feitas para corrigir erros de lint.

--- src/tests/BottomMenu.test.js
+++ src/tests/BottomMenu.test.js
@@ -1,11 +1,10 @@
-
-import App from "../App";
-import React from "react";
-import renderWithRouter from "../helpers/renderWithRouter";
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
-import userEvent from "@testing-library/user-event";
 
 describe('Testes do Componente "BottomMenu.js"', () => {
   const srcImagesIcons = [
@@ -34,72 +33,84 @@ describe('Testes do Componente "BottomMenu.js"', () => {
     history.push(page);
     expect(footer).not.toBeInTheDocument();
   };
-  const checkPageRedirection = (htmlElement, history, urlPathname ) => {
+  const checkPageRedirection = (htmlElement, history, urlPathname) => {
     userEvent.click(htmlElement);
     const { location: { pathname } } = history;
     expect(pathname).toBe(urlPathname);
   };
-  it('Verifica se o componente "BottomMenu" e seus subcomponentes existem na página "/foods"', () => {
+  const STRING_TESTS_BOTTOM_MENU = 'Verifica se o componente "BottomMenu"';
+  it(`${STRING_TESTS_BOTTOM_MENU} existe na página "/foods"'`, () => {
     checkBottomMenuDoesExist('/foods');
   });
-  it('Verifica se o componente "BottomMenu" e seus subcomponentes existem na página "/drinks"', () => {
+  it(`${STRING_TESTS_BOTTOM_MENU} existe na página "/drinks"'`, () => {
     checkBottomMenuDoesExist('/drinks');
   });
-  it('Verifica se o componente "BottomMenu" e seus subcomponentes existem na página "/explore"', () => {
+  it(`${STRING_TESTS_BOTTOM_MENU} existe na página "/explore"`, () => {
     checkBottomMenuDoesExist('/explore');
   });
-  it('Verifica se o componente "BottomMenu" e seus subcomponentes existem na página "/explore/foods"', () => {
+  it(`${STRING_TESTS_BOTTOM_MENU} existe na página "/explore/foods"`, () => {
     checkBottomMenuDoesExist('/explore/foods');
   });
-  it('Verifica se o componente "BottomMenu" e seus subcomponentes existem na página "/explore/drinks"', () => {
+  it(`${STRING_TESTS_BOTTOM_MENU} existe na página "/explore/drinks"`, () => {
     checkBottomMenuDoesExist('/explore/drinks');
   });
-  it.skip('Verifica se o componente "BottomMenu" e seus subcomponentes\n existem na página "/explore/foods/ingredients"', () => {
+  it.skip(`${STRING_TESTS_BOTTOM_MENU}
+    ${'existe na página "/explore/foods/ingredients"'}`, () => {
     checkBottomMenuDoesExist('/explore/foods/ingredients');
   });
-  it.skip('Verifica se o componente "BottomMenu" e seus subcomponentes\n existem na página "/explore/drinks/ingredients"', () => {
+  it.skip(`${STRING_TESTS_BOTTOM_MENU}
+    ${'existe na página "/explore/drinks/ingredients"'}`, () => {
     checkBottomMenuDoesExist('/explore/drinks/ingredients');
   });
-  it.skip('Verifica se o componente "BottomMenu" e seus subcomponentes\n existem na página "/explore/foods/nationalities"', () => {
+  it.skip(`${STRING_TESTS_BOTTOM_MENU}
+    ${'existe na página "/explore/foods/nationalities"'}`, () => {
     checkBottomMenuDoesExist('/explore/foods/nationalities');
   });
-  it('Verifica se o componente "BottomMenu" NÃO existe na página "/"', () => {
+  it(`${STRING_TESTS_BOTTOM_MENU} NÃO existe na página "/"`, () => {
     checkBottomMenuDoesNotExist('/');
   });
-  it('Verifica se o componente "BottomMenu" NÃO existe na página "/done-recipes"', () => {
+  it(`${STRING_TESTS_BOTTOM_MENU} NÃO existe na página "/done-recipes"`, () => {
     checkBottomMenuDoesNotExist('/done-recipes');
   });
-  it('Verifica se o componente "BottomMenu" NÃO existe na página "/favorite-recipes"', () => {
+  it(`${STRING_TESTS_BOTTOM_MENU}
+    ${'NÃO existe na página "/favorite-recipes"'}`, () => {
     checkBottomMenuDoesNotExist('/favorite-recipes');
   });
-  it('Verifica se o componente "BottomMenu" NÃO existe na tela de detalhes de uma receita de comida.', () => {
+  it(`${STRING_TESTS_BOTTOM_MENU}
+    ${'NÃO existe na tela de detalhes de uma receita de comida.'}`, () => {
     checkBottomMenuDoesNotExist('/foods/52771');
   });
-  it('Verifica se o componente "BottomMenu" NÃO existe na tela de detalhes de uma receita de bebida.', () => {
+  it(`${STRING_TESTS_BOTTOM_MENU}
+    ${'NÃO existe na tela de detalhes de uma receita de bebida.'}`, () => {
     checkBottomMenuDoesNotExist('/drinks/178319');
   });
-  it('Verifica se o componente "BottomMenu" NÃO existe na tela de receita em progresso de comida.', () => {
+  it(`${STRING_TESTS_BOTTOM_MENU}
+    ${'NÃO existe na tela de receita em progresso de comida.'}`, () => {
     checkBottomMenuDoesNotExist('/foods/52771/in-progress');
   });
-  it('Verifica se o componente "BottomMenu" NÃO existe na tela de receita em progresso de bebida.', () => {
+  it(`${STRING_TESTS_BOTTOM_MENU}
+    ${'NÃO existe na tela de receita em progresso de bebida.'}`, () => {
     checkBottomMenuDoesNotExist('/drinks/178319/in-progress');
   });
-  it('Verifica se redireciona para a rota correta, "/drinks", ao clicar\n no ícone de "CockTail" (drinkIcon.svg).', () => {
+  it(`${'Verifica se redireciona para a rota correta, "/drinks", ao clicar'}
+    ${'no ícone de "CockTail" (drinkIcon.svg).'}`, () => {
     const { getByTestId, history } = renderWithRouter(<App />);
     history.push('/foods');
     const drinksBottomBtn = getByTestId('drinks-bottom-btn');
-    checkPageRedirection(drinksBottomBtn, history, '/drinks' );
+    checkPageRedirection(drinksBottomBtn, history, '/drinks');
   });
-  it('Verifica se redireciona para a rota correta, "/foods", ao clicar\n no ícone de "Refeição" (mealIcon.svg).', () => {
+  it(`${'Verifica se redireciona para a rota correta, "/foods", ao clicar'}
+    ${'no ícone de "Refeição" (mealIcon.svg).'}`, () => {
     const { getByTestId, history } = renderWithRouter(<App />);
     history.push('/drinks');
     const foodBottomBtn = getByTestId('food-bottom-btn');
-    checkPageRedirection(foodBottomBtn, history, '/foods' );
+    checkPageRedirection(foodBottomBtn, history, '/foods');
   });
-  it('Verifica se redireciona para a rota correta, "/explore", ao clicar\n no ícone de "Bússola" (exploreIcon.svg).', () => {
+  it(`${'Verifica se redireciona para a rota correta, "/explore", ao clicar'}
+    ${'no ícone de "Bússola" (exploreIcon.svg).'}`, () => {
     const { getByTestId, history } = renderWithRouter(<App />);
     history.push('/foods');
     const exploreBottomBtn = getByTestId('explore-bottom-btn');
-    checkPageRedirection(exploreBottomBtn, history, '/explore' );
+    checkPageRedirection(exploreBottomBtn, history, '/explore');
   });
 });
