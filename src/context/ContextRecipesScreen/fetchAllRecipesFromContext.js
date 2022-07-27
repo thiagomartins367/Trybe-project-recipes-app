03802184ae5fb9aa914b6d555fa07e421006c0ae
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-27 01:35:45 -0300

    style(fetchAllRecipesFromContext.js): :art: Alterações feitas para corrigir formatação de código

--- src/context/ContextRecipesScreen/fetchAllRecipesFromContext.js
+++ src/context/ContextRecipesScreen/fetchAllRecipesFromContext.js
@@ -7,12 +7,7 @@ const fetchAllRecipesFromContext = async (typeOfRecipes, categories) => {
   try {
     const filteredRecipes = [];
     let index = 0;
-    // console.log('typeOfRecipes from fetchAllRecipesFromContext: ', typeOfRecipes);
     const testFunction = async () => {
-      // console.log(
-      //   'Link ---> ',
-      //   `https://www.the${typeOfRecipes}db.com/api/json/v1/1/filter.php?c=${categories[index].strCategory}`,
-      // );
       await fetchRecipesCategoriesAPI(
         `https://www.the${typeOfRecipes}db.com/api/json/v1/1/filter.php?c=${categories[index].strCategory}`,
       ).then((data) => {
@@ -20,7 +15,6 @@ const fetchAllRecipesFromContext = async (typeOfRecipes, categories) => {
           typeOfRecipes === 'meal'
             ? 'meals' : 'drinks'
         ]);
-        // console.log('fetchAllRecipesFromContext ---> data: ', data);
       });
       if (index !== categories.length - 1) {
         index += 1;
@@ -28,11 +22,10 @@ const fetchAllRecipesFromContext = async (typeOfRecipes, categories) => {
       }
     };
     await testFunction();
-    // console.log('return fetchAllRecipesFromContext ---> ', filteredRecipes);
 
     return filteredRecipes;
   } catch (error) {
-    // console.log('ERROR from fetchAllRecipesFromContext: ', error);
+    alert('Error getting data from Recipes !');
   }
 };
 
