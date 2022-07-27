050421f9a588f0ecd1b0af8d8c73a9b8227fbdeb
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-27 01:39:26 -0300

    feat(recomendationRecipe.js): Implementação da lógica de randomizar recomendações de receitas no "carousel"

--- src/components/recipesDetails/recomendationRecipe.js
+++ src/components/recipesDetails/recomendationRecipe.js
@@ -4,7 +4,7 @@ import { CardGroup } from 'react-bootstrap';
 import fetchRecipesAPI from '../../services/fetchRecipesAPI';
 import RecipeCard from './recipeCard';
 
-const NUMBER_FIVE = 5;
+const QUANTITY_RECOMMENDATIONS = 3;
 
 function RecomentationRecipe({ urlRecipesApi, stateContext, setStateContext }) {
   useEffect(() => {
@@ -15,8 +15,26 @@ function RecomentationRecipe({ urlRecipesApi, stateContext, setStateContext }) {
   const { drinks } = stateContext;
   const { meals } = stateContext;
 
+  const chosenIndices = [];
   const arrFilter = (drinks || meals) && (drinks || meals)
-    .filter((_recipe, index) => index <= NUMBER_FIVE);
+    .map((_element, elementIndex, array) => {
+      let result = null;
+      if (elementIndex < QUANTITY_RECOMMENDATIONS) {
+        let randomIndex = 0;
+        for (let index = 0; index < 1; index += 1) {
+          randomIndex = Math.floor(Math.random() * array.length);
+          randomIndex = randomIndex === array.length ? array.length - 1 : randomIndex;
+          chosenIndices.push(randomIndex);
+          if (chosenIndices.includes(randomIndex)) {
+            index = 0;
+          }
+        }
+        result = array[randomIndex];
+      }
+      return result;
+    })
+    .filter((recipe) => recipe !== null)
+    .filter((_recipe, index) => index < QUANTITY_RECOMMENDATIONS);
 
   return (
     <div className="carousel">
@@ -25,6 +43,7 @@ function RecomentationRecipe({ urlRecipesApi, stateContext, setStateContext }) {
           <RecipeCard
             key={ recipe.idDrink || recipe.idMeal }
             title={ recipe.strDrink || recipe.strMeal }
+            classCards="recomendation-card"
             image={ recipe.strDrinkThumb || recipe.strMealThumb }
             category={ recipe.strAlcoholic || recipe.strCategory }
             testid={ `${[i]}-recomendation-card` }
